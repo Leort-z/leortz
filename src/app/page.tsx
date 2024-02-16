e579ec6342734466e80e70e0ab3f44e4ae750db0
Leonardo Teixeira <leonardorteixeira@hotmail.com>
2024-02-16 17:21:13 -0300

    feat: about section improved and skills/experiences section added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
-import { PinIcon } from './icons'
+import { Badge } from '@/components'
+import { PinIcon } from '../icons'
 
 export default function Home() {
   return (
@@ -17,18 +18,62 @@ export default function Home() {
           </a>
         </div>
       </section>
-      <section className='flex flex-col space-y-2'>
+      <section className='flex flex-col space-y-2 text-justify'>
         <h2 className='text-xl font-bold'>About</h2>
         <p className='font-mono text-sm text-gray-700'>
-        As a Software Engineer, I excel in transforming complex challenges into intuitive and useful solutions. 
-        My expertise primarily lies in JavaScript/TypeScript, React and Java/Kotlin, with a special focus on frontend technologies. 
-        I have contributed to multiple projects across a variety of business sectors, including compliance, construction, privacy, agriculture, and health.
+          As a Software Engineer, I excel in transforming complex challenges into intuitive and useful solutions. My
+          expertise primarily lies in React, JavaScript/TypeScript, and Java/Kotlin, with a special focus on Frontend
+          technologies. I have contributed to multiple projects across a variety of business sectors, including
+          compliance, construction, privacy, agriculture and health.
         </p>
         <p className='font-mono text-sm text-gray-700'>
-        I approach product development with a keen sense of critical analysis, always aiming to achieve the best user experience and to deliver significant value to businesses.
-        Committed to continuous learning, I stay up to date with the latest technological trends to ensure my work remains at the forefront of industry standards.
-        My collaborative approach, combined with my ability to lead teams, has enabled successful project outcomes and made positive impacts across each sector.
+          I approach product development with a keen sense of critical analysis, always aiming to achieve the best user
+          experience and to deliver significant value to businesses. Committed to continuous learning, I stay up to date
+          with the latest technological trends to ensure my work remains at the forefront of industry standards. My
+          collaborative approach, combined with my ability to lead teams, has enabled successful project outcomes and
+          made positive impacts across each sector.
         </p>
+      </section>
+
+      <section className='flex flex-col space-y-2'>
+        <h2 className='text-xl font-bold'>Skills</h2>
+        <div className='flex gap-2'>
+          <Badge>HTML</Badge>
+          <Badge>CSS</Badge>
+          <Badge>JavaScript</Badge>
+          <Badge>TypeScript</Badge>
+          <Badge>React</Badge>
+          <Badge>Next</Badge>
+          <Badge>Java</Badge>
+          <Badge>Kotlin</Badge>
+          <Badge>Spring</Badge>
+        </div>
+      </section>
+
+      <section className='flex flex-col space-y-2'>
+        <h2 className='text-xl font-bold'>Experience</h2>
+        <div className='flex items-center justify-between'>
+          <h3>
+            <a href='https://www.wswork.com.br/' target='_blank' className='text-md font-bold hover:underline'>
+              WS Work
+            </a>
+          </h3>
+          <div className='text-sm tabular-nums text-gray-500'>2021 - Present</div>
+        </div>
+        <p className='font-mono text-sm text-gray-700'>Mid Level Full Stack Developer</p>
+      </section>
+
+      <section className='flex flex-col space-y-2'>
+        <h2 className='text-xl font-bold'>Education</h2>
+        <div className='flex items-center justify-between'>
+          <h3>
+            <a href='https://www.unisinos.br/' target='_blank' className='text-md font-bold hover:underline'>
+              Universidade do Vale do Rio dos Sinos
+            </a>
+          </h3>
+          <div className='text-sm tabular-nums text-gray-500'>2017 - 2022</div>
+        </div>
+        <p className='font-mono text-sm text-gray-700'>Bachelor&apos;s Degree in Computer Science</p>
       </section>
     </main>
   )
---
+++ src/components/badge.tsx
@@ -0,0 +1,3 @@
+export function Badge({ children }: { children: React.ReactNode }) {
+  return <div className='bg-zinc-900 text-white text-xs px-2 py-0.5 rounded font-semibold font-mono'>{children}</div>
+}
